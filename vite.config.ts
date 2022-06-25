62dc3c7918eeadd02290fce0e90a435795d8204a
Le Dinh <64189902+dinhdjj@users.noreply.github.com>
2022-06-25 19:48:32 +0700

    build: migrating from `laravel-mix` to `vite`
    Support typescript

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import laravel from "vite-plugin-laravel";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            postcss: [],
+        }),
+    ],
+});
