79fd025d7898006283c38ba5143e116d220bbb75
Le Dinh <64189902+dinhdjj@users.noreply.github.com>
2022-06-25 21:22:37 +0700

    chore(linting): uses `eslint`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 import laravel from "vite-plugin-laravel";
 
 export default defineConfig({
     plugins: [
+        eslint(),
         laravel({
             postcss: [],
         }),
