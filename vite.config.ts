551515e7c3bb57e9984aab62c5a8dee4f7fbc8da
Le Dinh <64189902+dinhdjj@users.noreply.github.com>
2022-06-29 20:15:44 +0700

    build: resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import eslint from "vite-plugin-eslint";
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+            '~': '/resources/js/Components'
+        }
+    },
     plugins: [
         eslint(),
         laravel([
