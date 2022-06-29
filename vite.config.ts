6c671605606f4ce166a60d7e6aaf3cbfa54fa840
Le Dinh <64189902+dinhdjj@users.noreply.github.com>
2022-06-29 19:57:55 +0700

    build: migrated

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
-import laravel from "vite-plugin-laravel";
-import tailwindcss from "tailwindcss"
-import autoprefixer from "autoprefixer"
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         eslint(),
-        laravel({
-            postcss: [tailwindcss(), autoprefixer()],
-        }),
+        laravel([
+            'resources/js/app.ts',
+            'resources/css/app.css',
+        ]),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
     ],
 });
